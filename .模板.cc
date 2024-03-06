c2ad46797d20f2034cce904b03d4f0ed67f0495c
Huffer342-WSH <718007138@qq.com>
2024-03-07 00:17:38 +0800

    新增基本模板

---
+++ .模板.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int N = 1005;
+
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    return 0;
+}
